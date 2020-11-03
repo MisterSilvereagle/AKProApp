a83ecf46ade995358c63377afabcb15e810ed9c3
Krachkiste <64702343+Krachkiste@users.noreply.github.com>
2020-11-03 17:19:25 +0100

    Create AKPro.cpp

---
+++ Windows/bin/AKPro.cpp
@@ -0,0 +1,8 @@
+// AKPro.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Java Programms.
+
+#include <iostream>
+
+int main()
+{
+    std::system("java -jar AKProUpdater.jar");
+}
