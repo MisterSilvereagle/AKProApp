c12c89bee789a65e041f2176952184c92377cecc
Krachkiste <64702343+Krachkiste@users.noreply.github.com>
2020-11-03 17:21:07 +0100

    Delete AKPro.cpp

--- Windows/AKPro.cpp
+++
@@ -1,8 +0,0 @@
-// AKPro.cpp : Diese Datei enthält die Funktion "main". Hier beginnt und endet die Ausführung des Java Programms.
-
-#include <iostream>
-
-int main()
-{
-    std::system("java -jar AKProUpdater.jar");
-}
